66417f2266b79a0b873d1676b6f0583ee7c8466e
hey2022 <he.yiheng@outlook.com>
2022-02-09 03:10:08 +0800

    added multistep factoring quadratics

--- Factoring_Quadratics.cpp
+++ Factoring_Quadratics.cpp
@@ -1,5 +1,4 @@
 #include <iostream>
-#include <cmath>
 
 int * find(int a, int b, int c, int range) {
     for (int i = range; i >= -range; --i) {
@@ -16,15 +15,29 @@ int * find(int a, int b, int c, int range) {
     }
 }
 
-void print_ans(int * ans, int a) {
-    std::cout <<"(" << ans[0] << "x" << std::showpos << ans[1] << ")" << "(" << std::noshowpos << ans[2] << "x" << std::showpos << ans[3] << ")" << std::endl;
+void print_ans(int * ans, int gcf) {
+    std::cout << gcf <<"(" << ans[0] << "x" << std::showpos << ans[1] << ")" << "(" << std::noshowpos << ans[2] << "x" << std::showpos << ans[3] << ")" << std::endl;
 }
+
+int GCF (int a, int b, int c) {
+    int max;
+    max = std::max(a, std::max(b, c));
+    for(int i = max; i >= 1; --i) {
+        if((a % i == 0 ) and (b % i == 0 ) and (c % i == 0 )) {
+            return i;
+        }
+    }
+    return 0;
+}
+
 
 
 int main() {
     int a, b, c, range = 50;
     std::cin >> a >> b >> c;
+    int gcf = GCF(a, b, c);
+    a /= gcf, b /= gcf, c /= gcf;
     int * ans = find(a, b, c, range);
-    print_ans(ans, a);
+    print_ans(ans, gcf);
     return 0;
 }
