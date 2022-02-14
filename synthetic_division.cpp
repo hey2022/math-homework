8b34c1a993492de4739b8b5c8ebc54555e71d4b1
Jamey <48553457+hey2022@users.noreply.github.com>
2022-02-14 20:07:30 +0800

    Complete

--- synthetic_division.cpp
+++ synthetic_division.cpp
@@ -1,5 +1,29 @@
 #include <iostream>
 
+void output(float answer[], int degree, float devisor) {
+    for (int i = 0; i < degree; ++i) {
+        std::cout << std::showpos << answer[i] << "x^" << std::noshowpos <<degree - 1 - i; 
+    }
+    std::cout << std::showpos << "+\\frac{" << answer[degree] << "}{x" << devisor << "}" << std::endl;
+ }
+
+void synthetic_division(float coefficient[], float devisor, int degree) {
+    float devisor_0 = -devisor, answer[degree + 1], temp = 0;
+    for (int i = 0; i <= degree; ++i) {
+        answer[i] = coefficient[i] + temp;
+        temp = devisor_0 * answer[i];
+    }
+    output(answer, degree, devisor);
+}
+
 int main() {
-  return 0;
+    int degree;
+    std::cin >> degree;
+    float coefficient[degree + 1], devisor;
+    for (int i = 0; i <= degree; ++i) {
+        std::cin >> coefficient[i];
+    }
+    std::cin >> devisor;
+    synthetic_division(coefficient, devisor, degree);
+    return 0;
 }
