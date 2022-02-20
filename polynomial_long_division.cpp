12cac41ed2948b1d385ba8dcabc099ff9a01d0c8
hey2022 <48553457+hey2022@users.noreply.github.com>
2022-02-20 21:46:20 +0800

    flip pointers

--- polynomial_long_division.cpp
+++ polynomial_long_division.cpp
@@ -1,36 +1,17 @@
 #include <iostream>
 
-void print(int degree, int index, const float * array) {
-    if (array[index] != 0) {
-        if (degree == 0) {
-            std::cout << std::showpos << array[index];
-        } else if (degree == 1) {
-            std::cout << std::showpos << array[index] << "x";
-        } else {
-            std::cout << std::showpos << array[index] << "x^" << std::noshowpos << degree;
-        }
+int main() {
+    int degree, divisor_degree;
+    std::cin >> degree;
+    float dividend[degree + 1];
+    for (int i = 0; i <= degree; ++i) {
+        std::cin >> dividend[i];
     }
-}
-
-void output(int degree, float * quotient, int divisor_degree, const float * divisor, float * dividend) {
-    for (int i = 0; i <= degree - divisor_degree; ++i) {
-        int var_degree = degree - divisor_degree - i;
-        print(var_degree, i, quotient);
-    }
-    std::cout << "+\\frac{";
-    for (int i = degree - divisor_degree; i <= degree; ++i) {
-        int var_degree = degree - i;
-        print(var_degree, i, dividend);
-    }
-    std::cout << "}{";
+    std::cin >> divisor_degree;
+    float divisor[divisor_degree + 1];
     for (int i = 0; i <= divisor_degree; ++i) {
-        int var_degree = divisor_degree - i;
-        print(var_degree, i, divisor);
+        std::cin >> divisor[i];
     }
-    std::cout << "}" << std::endl;
-}
-
-void polynomial_long_division(int degree, float * dividend, int divisor_degree, const float * divisor) {
     float quotient[degree - divisor_degree + 1];
     for (int i = 0; i <= degree - divisor_degree; ++i) {
         quotient[i] = dividend[i] / divisor[0];
@@ -38,36 +19,47 @@ void polynomial_long_division(int degree, float * dividend, int divisor_degree, 
             dividend[i + j] -= divisor[j] * quotient[i];
         }
     }
-    output(degree, quotient, divisor_degree, divisor, dividend);
+    for (int i = 0; i <= degree - divisor_degree; ++i) {
+        int var_degree = degree - divisor_degree - i;
+            if (quotient[i] != 0) {
+            if (var_degree == 0) {
+                std::cout << std::showpos << quotient[i];
+            } else if (var_degree == 1) {
+                std::cout << std::showpos << quotient[i] << "x";
+            } else {
+                std::cout << std::showpos << quotient[i] << "x^" << std::noshowpos << var_degree;
+            }
+        }
+    }
+    if (dividend[degree] != 0) {
+        std::cout << "+\\frac{";
+        for (int i = degree - divisor_degree; i <= degree; ++i) {
+            int var_degree = degree - i;
+            if (dividend[i] != 0) {
+                if (var_degree == 0) {
+                    std::cout << std::showpos << dividend[i];
+                } else if (var_degree == 1) {
+                    std::cout << std::showpos << dividend[i] << "x";
+                } else {
+                    std::cout << std::showpos << dividend[i] << "x^" << std::noshowpos << var_degree;
+                }
+            }
+        }
+        std::cout << "}{";
+        for (int i = 0; i <= divisor_degree; ++i) {
+            int var_degree = divisor_degree - i;
+            if (divisor[i] != 0) {
+                if (var_degree == 0) {
+                    std::cout << std::showpos << divisor[i];
+                } else if (var_degree == 1) {
+                    std::cout << std::showpos << divisor[i] << "x";
+                } else {
+                    std::cout << std::showpos << divisor[i] << "x^" << std::noshowpos << var_degree;
+                }
+            }
+        }
+        std::cout << "}" << std::endl;
+    }
 }
 
-float ** input() {
-    static float * input[4];
-    int degree, divisor_degree;
-    std::cout << "Degree of polynomial:";
-    std::cin >> degree;
-    std::cout << "Coefficients of polynomial including 0:";
-    float dividend[degree + 1];
-    for (int i = 0; i <= degree; ++i) {
-        std::cin >> dividend[i];
-    }
-    std::cout << "Degree of polynomial divisor:";
-    std::cin >> divisor_degree;
-    std::cout << "Coefficients of polynomial divisor including 0:";
-    float divisor[divisor_degree + 1];
-    for (int i = 0; i < divisor_degree; ++i) {
-        std::cin >> divisor[i];
-    }
-    auto f_degree = float (degree), f_divisor_degree = float (divisor_degree);
-    input[0] = &f_degree;
-    input[1] = dividend;
-    input[2] = &f_divisor_degree;
-    input[3] = divisor;
-    return input;
-}
 
-int main() {
-    float ** user_input = input();
-    polynomial_long_division(int (* user_input[0]), user_input[1], int (* user_input[2]), user_input[3]);
-    return 0;
-}
