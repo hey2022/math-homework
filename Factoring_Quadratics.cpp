3c57df2ab33205cab023e2af3aafae8e5c82e29c
Jamey <48553457+hey2022@users.noreply.github.com>
2022-03-18 20:31:18 +0800

    Update Factoring_Quadratics.cpp

--- Factoring_Quadratics.cpp
+++ Factoring_Quadratics.cpp
@@ -1,58 +1,60 @@
 #include <iostream>
 #include <cmath>
 
-long double  quadratic_formula(int a, int b, int c) {
-    long double delta = pow(b, 2) - 4 * a * c;
-    if (delta < 0) {
-        return nullptr;
-    } else if (delta == 0) {
-        static long double ans[1] = {(long double) (-b) / (2 * a)};
-        return ans;
-    }
-    static long double ans[2] = {(long double) (-b + sqrtl(delta )) / (2 * a), };
-}
+std::string root(int radicand);
+std::string quadratic_formula(int a, int b, int c);
+int HCF (int a, int b);
 
-int ans_
-
-int * factor(int a, int b, int c, int range) {
-    for (int i = range; i >= -range; --i) {
-        for (int j = range; j >= -range; --j) {
-            for (int k = range; k >= -range; --k) {
-                for (int l = range; l >= -range; --l) {
-                    if (i * k == a && j * l == c && i * l + j * k == b) {
-                        static int ans[4] = {i, j, k, l};
-                        return ans;
-                    }
-                }
-            }
-        }
-    }
-}
-
-void print_ans(int * ans, int gcf) {
-    std::cout << gcf <<"(" << ans[0] << "x" << std::showpos << ans[1] << ")" << "(" << std::noshowpos << ans[2] << "x" << std::showpos << ans[3] << ")" << std::endl;
-}
-
-int HCF (int a, int b, int c) {
-    a = abs(a), b = abs(b), c = abs(c);
-    int min;
-    min = std::min(a, std::min(b, c));
-    for(int i = min; i >= 1; --i) {
-        if((a % i == 0 ) and (b % i == 0 ) and (c % i == 0 )) {
-            return i;
-        }
-    }
+int main() {
+    int a, b, c;
+    std::cin >> a >> b >> c;
+    std::cout << quadratic_formula(a, b, c);
     return 0;
 }
 
+std::string root(int radicand, int a, int b, int c) {
+    int coefficient = 1, index = 2, d = 2;
+    while (std::pow(d, index) <= radicand) {
+        if (radicand % int(std::pow(d, index)) == 0) {
+            radicand /= (std::pow(d, index));
+            coefficient *= d;
+        } else {
+            ++d;
+        }
+    }
+    if (radicand == 1 && -b % (2 * a) == 0) {
+        return std::to_string(coefficient / 2 / a) + "\\sqrt{" + std::to_string(radicand) + "}";
+    } else {
+        int hcf = HCF(coefficient, 2 * a);
+        return "\\frac{" + std::to_string(coefficient / hcf) + "\\sqrt{" + std::to_string(radicand) + "}}{" + std::to_string(2 * a / hcf) + "}";
+    }
+}
 
+std::string quadratic_formula(int a, int b, int c) {
+    int radicand = pow(b, 2) - 4 * a * c;
+    if (radicand < 0) {
+        return "No roots";
+    } else if (radicand == 0 && -b % (2 * a) == 0) {
+        return std::to_string(double(-b) / (2 * a));
+    } else if (radicand == 0) {
+        return "\\frac{" + std::to_string(-b) + "}" + "{" + std::to_string(2 * a) + "}";
+    } else {
+        return "\\frac{" root(radicand, a, b, c);
+    }
+}
 
-int main() {
-    int a, b, c, range = 50;
-    std::cin >> a >> b >> c;
-    int hcf = HCF(a, b, c);
-    a /= hcf, b /= hcf, c /= hcf;
-    int * ans = factor(a, b, c, range);
-    print_ans(ans, hcf);
+// void print_ans(int * ans, int gcf) {
+//     std::cout << gcf <<"(" << ans[0] << "x" << std::showpos << ans[1] << ")" << "(" << std::noshowpos << ans[2] << "x" << std::showpos << ans[3] << ")" << std::endl;
+// }
+
+int HCF (int a, int b) {
+    a = abs(a), b = abs(b);
+    int min;
+    min = std::min(a, b);
+    for(int i = min; i >= 1; --i) {
+        if((a % i == 0 ) and (b % i == 0 )) {
+            return i;
+        }
+    }
     return 0;
 }
